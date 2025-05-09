0ae9d32236620f2ce5076b525270b269184b42f8
Hamed <hamed@niroomand.dev>
2025-05-09 21:58:15 +0330

    feat: integrate Fastify with NestJS and enhance server configuration

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,51 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import compression from '@fastify/compress';
+import helmet from '@fastify/helmet';
+import { join } from 'path';
+import fastifyStatic from '@fastify/static';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    { cors: false },
+  );
+
+  const fastifyInstance = app.getHttpAdapter().getInstance();
+
+  await app.register(helmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", 'data:', 'blob:'],
+        connectSrc: ["'self'", 'ws:', 'wss:'],
+        fontSrc: ["'self'"],
+        objectSrc: ["'none'"],
+        mediaSrc: ["'self'"],
+        frameSrc: ["'self'"],
+      },
+    },
+  });
+
+  await app.register(compression, { encodings: ['gzip', 'deflate'] });
+
+  await fastifyInstance.register(fastifyStatic, {
+    root: join(__dirname, '../../client/public'),
+    prefix: '/',
+    decorateReply: false,
+    serve: true,
+  });
+
+  await app.listen(process.env.PORT ?? 3030, '0.0.0.0');
+  const appUrl = await app.getUrl();
+  console.log(`Server is running on ${appUrl}`);
 }
-bootstrap();
+
+bootstrap().catch(console.error);

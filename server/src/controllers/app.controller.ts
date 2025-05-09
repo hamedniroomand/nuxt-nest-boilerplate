1e76631e35fbddb623f2a3ab8822b25eb59e5be5
Hamed <hamed@niroomand.dev>
2025-05-10 03:22:12 +0330

    refactor: remove unused serveAll method from AppController to simplify code

--- server/src/controllers/app.controller.ts
+++ server/src/controllers/app.controller.ts
@@ -1,14 +1,4 @@
-import { Controller, Get, Res } from '@nestjs/common';
-import { join } from 'path';
-import { FastifyReply } from 'fastify';
-import { readFileSync } from 'fs';
+import { Controller } from '@nestjs/common';
 
 @Controller()
-export class AppController {
-  @Get('*')
-  serveAll(@Res() reply: FastifyReply) {
-    const filePath = join(__dirname, '../../../client/public/200.html');
-    const content = readFileSync(filePath, 'utf-8');
-    return reply.type('text/html').send(content);
-  }
-}
+export class AppController {}

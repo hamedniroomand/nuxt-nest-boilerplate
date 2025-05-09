2095173bb4cf9724f4993a2d84e15b1cee5cbe43
Hamed <hamed@niroomand.dev>
2025-05-10 03:26:50 +0330

    feat: create index page with a greeting and link to users, and update users page with links to individual users

---
+++ client/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <ul>
+      <li>
+        <nuxt-link to="/users">Users</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
--- client/pages/users/index.vue
+++ client/pages/users/index.vue
@@ -1,3 +1,14 @@
 <template>
   <p>Users</p>
+  <ul>
+    <li>
+      <nuxt-link to="/users/1">User 1</nuxt-link>
+    </li>
+    <li>
+      <nuxt-link to="/users/2">User 2</nuxt-link>
+    </li>
+    <li>
+      <nuxt-link to="/users/3">User 3</nuxt-link>
+    </li>
+  </ul>
 </template>
